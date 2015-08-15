c7fcc0bf4e1e93eeb8696e2f4578f09143967801
giulio <giuliop@gmail.com>
2015-08-15 17:57:49 +0200

    added automatic load of repl utilities at repl session

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,7 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.9.1"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+  :repl-options
+    {:init
+     (do (load-file (str (System/getenv "HOME") "/dev/clojure/lib/gws/src/repl.clj"))
+         (require '[gws.repl :as u]))}}}
